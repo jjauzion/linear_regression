0c5f6d89b65607311af80c3264a890289847a5d5
jjauzion <jjauzion@student.42.fr>
2019-08-15 18:01:37 +0200

    v1

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 def mean_normalization(data):
@@ -40,14 +41,15 @@ def update_weight(weight, X, Y, learning_rate):
     :param weight: n by 1 matrix
     :param learning_rate: learning rate
     :return: updated weight as a n by 1 matrix
-    """
+
     tmp = np.matmul(X.transpose(), hypothesis(weight, X) - Y)
     grad = learning_rate / X.shape[0] * tmp
     print("grad : \n{}".format(grad))
     new = weight - grad
     print("weight : \n{}".format(weight))
     return new
-    # return weight - learning_rate / X.shape[0] * np.matmul(X.transpose(), predict(weight, X) - Y)
+    """
+    return weight - learning_rate / X.shape[0] * np.matmul(X.transpose(), hypothesis(weight, X) - Y)
 
 
 def linear_regression(X, Y, nb_iter, learning_rate):
@@ -58,20 +60,20 @@ def linear_regression(X, Y, nb_iter, learning_rate):
     :param learning_rate: learning rate
     :return: tuple (weight, cost_history) with weight as a n by 1 matrix and cost_history as a list
     """
-    X, avg, std = mean_normalization(X)
     X = np.insert(X, 0, np.ones(X.shape[0]), axis=1)
     weight = np.random.random((X.shape[1], 1))
     cost_history = []
     for i in range(nb_iter):
         cost_history.append(cost(weight, X, Y))
         weight = update_weight(weight, X, Y, learning_rate)
-    return weight, cost_history, avg, std
+    final_hyp = hypothesis(weight, X)
+    accuracy = np.average(abs(final_hyp - Y))
+    return weight, cost_history, accuracy
 
 
 def predict(mileage, weight, average, std_dev):
-    x = mileage * average / std_dev
+    x = (mileage - average) / std_dev
     price = weight[0] + x * weight[1]
-    print("price = {}".format(price))
     return price
 
 
@@ -83,8 +85,15 @@ if __name__ == "__main__":
     df = np.array(data[1:], dtype="float64")
     X = df[:, 0:1]
     Y = df[:, 1:2]
-    w, cost_hist, avg, std_dev = linear_regression(X, Y, 1000, 0.1)
-    print("result:")
+    X, avg, std_dev = mean_normalization(X)
+    w, cost_hist, accuracy = linear_regression(X, Y, 200, 0.05)
+    print("weight:")
     print(w)
+    print("cost history:")
     print(cost_hist[-10:])
-    predict(23000, w, avg, std_dev)
+    print("accuracy")
+    print(accuracy)
+    plt.plot(cost_hist)
+    plt.show()
+    price = predict(60000, w, avg, std_dev)
+    print("price = {}".format(price))
